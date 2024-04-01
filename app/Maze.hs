d8ae64c17b30dfa42331bf916344d65d5ca102a3
Victor Buendia <victorbuendia03@gmail.com>
2024-04-01 17:52:00 -0300

    Cria funções de movimentação de ponto no labirinto com as direções

--- app/Maze.hs
+++ app/Maze.hs
@@ -40,3 +40,25 @@ inBounds :: Maze -> Coord -> Bool
 inBounds maze (x,y) = 
     x >= 0 && y >= 0 && x < length (row:rows) && y < length (row)
     where (row:rows) = maze
+
+getNeighborsDirections :: Maze -> Coord -> [Directions]
+getNeighborsDirections maze coord = map (findDirection coord) neighbors
+    where
+        neighbors = getNeighbors maze coord
+
+getNeighbors :: Maze -> Coord -> [Coord]
+getNeighbors maze (x,y) = filter (\coords -> (inBounds) maze coords) neighborCoords
+    where
+        neighborCoords :: [Coord]
+        neighborCoords = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+
+findDirection :: Coord -> Coord -> Directions
+findDirection start end
+    | (x1,y1) == (x0-1,y0) = ToUp
+    | (x1,y1) == (x0+1,y0) = ToDown
+    | (x1,y1) == (x0,y0+1) = ToRight
+    | (x1,y1) == (x0,y0-1) = ToLeft
+    | otherwise            = None
+    where
+        (x0,y0) = start
+        (x1,y1) = end
