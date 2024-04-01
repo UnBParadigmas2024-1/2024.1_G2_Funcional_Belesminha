b68cd3f9a44c1fb41274de0bc02dff06ad66440f
Victor Buendia <victorbuendia03@gmail.com>
2024-04-01 19:08:36 -0300

    Cria função que recupera vizinhos de um ponto no labirinto

--- app/Maze.hs
+++ app/Maze.hs
@@ -38,3 +38,14 @@ updateMaze maze (x,y) cll = (a ++ [l ++ cll:r] ++ b)
     where
         (a,row:b)  = splitAt x maze
         (l,col:r)  = splitAt y row
+
+getNeighbors :: Maze -> Coord -> [Coord]
+getNeighbors maze (x,y) = filter (\coords -> inBounds maze coords) neighborCoords
+    where
+        neighborCoords :: [Coord]
+        neighborCoords = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+
+        inBounds :: Maze -> Coord -> Bool
+        inBounds maze (x,y) = x >= 0 && y >= 0 && x < length maze && y < length cols
+            where
+                (cols:rows) = maze
