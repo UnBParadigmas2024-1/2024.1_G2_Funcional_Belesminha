2b97f0dae06a5515de6fb220d93e1a25c0d6b5f0
senaarth <senaarth@gmail.com>
2024-04-03 20:36:15 -0300

    Gera e desenha folhas no mapa
    Co-authored-by: Kevin <k.luis360@gmail.com>

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,21 @@
 import Graphics.Gloss
 import Map ( mazeMap, Cell(..) )
-import Maze ( Maze )
+import Maze ( Maze, generateLeaves, updateMaze )
 
 cellSize :: Float
 cellSize = 20
 
 cellToPicture :: Cell -> Picture
-cellToPicture Wall  = color black $ rectangleSolid cellSize cellSize
-cellToPicture Path  = color white $ rectangleSolid cellSize cellSize
-cellToPicture Start = color green $ rectangleSolid cellSize cellSize
-cellToPicture End   = color red   $ rectangleSolid cellSize cellSize
+cellToPicture Wall  = color black  $ rectangleSolid cellSize cellSize
+cellToPicture Path  = color white  $ rectangleSolid cellSize cellSize
+cellToPicture Start = color green  $ rectangleSolid cellSize cellSize
+cellToPicture End   = color red    $ rectangleSolid cellSize cellSize
+cellToPicture Leaf  = color orange $ rectangleSolid cellSize cellSize
 
-sampleWorld :: World
-sampleWorld = World {
-    worldMap = mazeMap
+
+sampleWorld :: Maze -> World
+sampleWorld maze = World {
+    worldMap = maze
 }
 
 data World = World {
@@ -31,11 +33,15 @@ mazeToPicture world =
     ]
 
 main :: IO ()
-main = play
-    (InWindow "Belesminha" (600,600) (0,0))
-    white
-    30
-    sampleWorld
-    mazeToPicture
-    (\_ nothing -> nothing)
-    (\_ nothing -> nothing)
+main = do
+    let initialMaze = mazeMap
+    leaves <- generateLeaves initialMaze
+    let mazeWithLeaves = foldl (\mz (x,y) -> updateMaze mz (x,y) Leaf) initialMaze leaves
+    play
+        (InWindow "Belesminha" (600,600) (0,0))
+        white
+        30
+        (sampleWorld mazeWithLeaves)
+        mazeToPicture
+        (\_ nothing -> nothing)
+        (\_ nothing -> nothing)
