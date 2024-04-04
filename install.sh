0077f5085efcfd79792a5cf38fad97c6d267b88a
ggomesbr <gabriel.luiz77888@gmail.com>
2024-04-04 17:32:10 -0300

    Atualização do install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
-#! /bin/bash
-
 #!/bin/bash
 
 OSTYPE=$(uname)
 
-if [ $OSTYPE = "Darwin" ];
-    then
-        brew install haskell-stack;
-        brew install cabal-install;
-        cabal install --lib gloss;
-elif [ $OSTYPE = "Linux "];
-    then
-        echo "OI"
+if [ "$OSTYPE" = "Darwin" ]; then
+    brew install haskell-stack
+    brew install cabal-install
+elif [ "$OSTYPE" = "Linux" ]; then
+    sudo apt-get update
+    sudo apt-get install haskell-platform
+    sudo apt-get install cabal-install
+else
+    echo "Windows"
+fi
+
+if [ "$OSTYPE" = "Linux" ] || [ "$OSTYPE" = "Darwin" ]; then
+    cabal update
+    cabal install --lib gloss
+    cabal install --lib random
 fi
