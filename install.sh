17d8d1d436a6f1e67a9c6fe4bee4ca7d46053092
papercut <papercut@papercut.doom>
2024-04-04 20:17:02 -0300

    Adicionando bibliotecas prerequisito para o gloss no linux

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ elif [ "$OSTYPE" = "Linux" ]; then
     sudo apt-get update
     sudo apt-get install haskell-platform
     sudo apt-get install cabal-install
+    
+    # Essas bibliotecas garantem que o OpenGL no sistema esteja de acordo com os requisitos para instalar o gloss
+    sudo apt-get install cmake pkg-config
+    sudo apt-get install mesa-utils libglu1-mesa-dev freeglut3-dev mesa-common-dev
+    sudo apt-get install libglew-dev libglfw3-dev libglm-dev
+    sudo apt-get install libao-dev libmpg123-dev
 else
     echo "Windows"
 fi
