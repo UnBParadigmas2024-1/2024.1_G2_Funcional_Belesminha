febd604bb71d1da3ab2cc5ee94b4988983fb7e03
Ailton Aires <ailtonaires5@gmail.com>
2024-04-04 10:30:15 -0300

    Algoritmo de Dijkstra sem peso buscando o menor caminho entre 2 pontos

--- dijkstra/myfirstapp/app/Main.hs
+++ dijkstra/myfirstapp/app/Main.hs
@@ -1,26 +1,66 @@
 module Main where
+import qualified Data.Set as Set
+import Data.Maybe (fromJust)
+import Data.List (minimumBy, foldl')
+import Data.Function (on)
 
-import Dijkstra (dijkstra)
+type Position = (Int, Int)
+data Cell = Wall | Path | Start | End deriving (Eq, Show)
 
-matriz :: [[Int]]
-matriz = [[0, 10, 10, 5, 0], [0, 0, 1, 2, 0], [0, 0, 0, 0, 4], [0, 3, 9, 0, 2], [7, 0, 6, 0, 0]]
+mazeMap :: [[Cell]]
+mazeMap =
+    [
+        [Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall]
+        ,[Wall,Start,Wall,Path,Path,Wall,Wall,Wall,Wall,Path,Path,Path,Wall,Wall,Path,Path,Path,Path,Path,Path,Path,Path,Path,Wall]
+        ,[Wall,Path,Wall,Path,Path,Path,Wall,Wall,Path,Path,Path,Path,Path,Path,Path,Path,Wall,Wall,Path,Path,Wall,Wall,Path,Wall]
+        ,[Wall,Path,Path,Path,Path,Path,Path,Path,Path,Path,Wall,Path,Path,Wall,Path,Path,Wall,Wall,Path,Wall,Wall,Path,Path,Wall]
+        ,[Wall,Path,Path,Path,Wall,Path,Path,Wall,Wall,Wall,Wall,Path,Path,Wall,Path,Path,Path,Path,Path,Path,Path,Path,Path,Wall]
+        ,[Wall,Path,Path,Wall,Wall,Wall,Path,Path,Path,Path,Path,Path,Path,Wall,Wall,Path,Path,Path,Path,Wall,Wall,Wall,Path,Wall]
+        ,[Wall,Path,Path,Path,Wall,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Wall,Path,Wall]
+        ,[Wall,Path,Wall,Path,Path,Path,Path,Wall,Wall,Wall,Path,Wall,Wall,Path,Path,Path,Wall,Path,Path,Wall,Wall,Wall,Path,Wall]
+        ,[Wall,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Wall,Wall,Path,Path,Path,Wall,Wall,Wall,Wall,Wall,Wall,Path,Wall]
+        ,[Wall,Path,Wall,Wall,Wall,Wall,Wall,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Wall]
+        ,[Wall,Path,Path,Path,Path,Path,Wall,Path,Path,Path,Path,Path,Path,Path,Wall,Path,Path,Path,Wall,Path,Path,Wall,Path,Wall]
+        ,[Wall,Path,Path,Wall,Wall,Path,Wall,Path,Path,Wall,Wall,Wall,Path,Path,Wall,Wall,Path,Path,Wall,Path,Wall,Wall,Path,Wall]
+        ,[Wall,Path,Path,Wall,Wall,Path,Wall,Path,Path,Wall,Path,Path,Path,Path,Path,Path,Path,Path,Wall,Path,Path,Path,Path,Wall]
+        ,[Wall,Path,Path,Path,Path,Path,Path,Path,Path,Wall,Path,Path,Path,Wall,Wall,Path,Path,Wall,Wall,Wall,Path,Path,Path,Wall]
+        ,[Wall,Path,Wall,Wall,Wall,Path,Path,Wall,Wall,Wall,Path,Wall,Wall,Wall,Wall,Path,Path,Path,Path,Path,Path,Path,Path,Wall]
+        ,[Wall,Path,Path,Path,Wall,Path,Path,Path,Path,Path,Path,Path,Wall,Wall,Path,Path,Path,Path,Path,Path,Wall,Wall,Path,Wall]
+        ,[Wall,Path,Path,Path,Wall,Wall,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Wall,Wall,Path,Path,Wall,Path,Path,Wall]
+        ,[Wall,Path,Path,Path,Path,Path,Path,Path,Path,Wall,Path,Path,Wall,Wall,Wall,Path,Wall,Wall,Path,Path,Path,Path,Path,Wall]
+        ,[Wall,Path,Wall,Path,Path,Path,Wall,Path,Wall,Wall,Wall,Path,Path,Wall,Path,Path,Path,Path,Path,Wall,Path,Path,Path,Wall]
+        ,[Wall,Path,Wall,Path,Path,Wall,Wall,Path,Path,Wall,Path,Path,Path,Wall,Wall,Wall,Path,Path,Wall,Wall,Wall,Wall,Path,Wall]
+        ,[Wall,Path,Wall,Path,Wall,Wall,Wall,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Wall]
+        ,[Wall,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Path,Wall,Path,Path,Path,Path,Path,End,Wall]
+        ,[Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall]
+    ]
+    
 
-frutas :: [Int]
-frutas = [1, 2, 3]
 
-lengthFrutas :: Int
-lengthFrutas = length frutas
+-- Retorna a lista de vizinhos válidos de uma posição
+getValidNeighbors :: Position -> [[Cell]] -> [Position]
+getValidNeighbors (x, y) maze = filter (\(x', y') -> x' >= 0 && y' >= 0 && x' < length maze && y' < length (head maze) && maze !! x' !! y' /= Wall) [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
+
+
+-- Algoritmo de Dijkstra
+dijkstra :: Position -> Position -> [[Cell]] -> Maybe [Position]
+dijkstra start end maze = dijkstra' (Set.singleton (0, start, [start])) Set.empty
+    where
+        dijkstra' :: Set.Set (Int, Position, [Position]) -> Set.Set Position -> Maybe [Position]
+        dijkstra' pq visited
+            | Set.null pq = Nothing
+            | otherwise = let ((cost, pos, path), pq') = Set.deleteFindMin pq in
+                if pos == end then Just path
+                else if Set.member pos visited then dijkstra' pq' visited
+                else let neighbors = getValidNeighbors pos maze in
+                    dijkstra' (foldl' (\acc neighbor -> Set.insert (cost + 1, neighbor, path ++ [neighbor]) acc) pq' neighbors) (Set.insert pos visited)
 
 main :: IO ()
 main = do
-  retorno <- dijkstra matriz frutas lengthFrutas
-  putStrLn $ "Retorno da função printMatriz: " ++ show retorno
-
-
-
-  -- printMatriz :: [[Int]] -> [Int] -> Int -> IO Int
--- printMatriz matriz frutas lengthFrutas = do
---   let frutasPermutations = permutations frutas
---   putStrLn "Frutas Permutations:"
---   print frutasPermutations
---   return 0
+  let frutas = [(5, 2), (13, 7), (20, 9)] -- Ou qualquer outra lista de frutas que você deseja testar
+  let start = (1, 1)
+  let end = (21, 22)
+  let path = dijkstra start (5, 2) mazeMap
+  case path of
+    Just p -> putStrLn $ "Caminho: " ++ show p
+    Nothing -> putStrLn "Não foi possível encontrar um caminho"
