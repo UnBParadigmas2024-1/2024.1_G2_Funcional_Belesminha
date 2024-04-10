9d22f401b00991e7c6812bc8de3c9b9847ce26e4
Victor Buendia <victorbuendia03@gmail.com>
2024-04-10 12:59:54 -0300

    Implementa contador de passos

--- app/World.hs
+++ app/World.hs
@@ -15,15 +15,20 @@ initializeWorld leavesList = do
         leaves = leavesList
     let mazeWithLeaves = foldl (\mz (x,y) -> updateMaze mz (x,y) Leaf) initialMaze leaves
         newWorld = World {
-            worldMap = mazeWithLeaves,
-            endPos = (23, 17),
-            startPos = (1, 1),
-            playerPos = (1, 1)
+            worldMap  = mazeWithLeaves,
+            endPos    = (23, 17),
+            startPos  = (1, 1),
+            playerPos = (1, 1),
+            moveCount = 0
         }
     return newWorld
 
 data World where
-  World :: {worldMap :: Maze, endPos :: Coord, startPos :: Coord, playerPos :: Coord} -> World
+  World :: { worldMap :: Maze, 
+        endPos :: Coord, 
+        startPos :: Coord, 
+        playerPos :: Coord,
+        moveCount :: Int } -> World
 
 mazeToPicture :: [Coord] -> World -> Picture
 mazeToPicture minSteps world =
@@ -33,7 +38,10 @@ mazeToPicture minSteps world =
         reversedMaze = reverse maze
         minStepsPictures = pictures [translate (fromIntegral x * cellSize) (fromIntegral y * cellSize) (color (makeColor 0 0 1 0.2) $ rectangleSolid cellSize cellSize) | (x, y) <- zip xa yb]
     in translate (-240) (-225) . pictures $
-                                    [ translate (x * cellSize) (y * cellSize) (cellToPicture cell) | (y,  row) <- zip [0..] reversedMaze , (x, cell) <- zip [0..] row ] ++ [minStepsPictures]
+                                    [ translate (x * cellSize) (y * cellSize) (cellToPicture cell) | (y,  row) <- zip [0..] reversedMaze , (x, cell) <- zip [0..] row ] ++ [minStepsPictures] ++ [drawStepCount (moveCount world)]
+
+drawStepCount :: Int -> Picture
+drawStepCount n = Translate (0) (-40) $ Scale 0.3 0.3 $ Color white $ Text $ "STEPS: " ++ show n
 
 changeDirection :: Event -> Directions
 changeDirection (EventKey (SpecialKey KeyDown) Down _ _)  = ToDown
@@ -42,14 +50,21 @@ changeDirection (EventKey (SpecialKey KeyLeft) Down _ _)  = ToLeft
 changeDirection (EventKey (SpecialKey KeyRight) Down _ _) = ToRight
 changeDirection _                                         = None
 
+incrementStep :: Coord -> Coord -> Int
+incrementStep (x1,y1) (x2,y2)
+    | x1 == x2 && y1 == y2 = 0
+    | otherwise = 1
+
 handleInput :: Event -> World -> World
 handleInput ev world =
-    World { worldMap = newMap', playerPos = newPos }
+    World { worldMap = newMap', playerPos = newPos, moveCount = stepsTaken+increase }
     where
         map = worldMap world
         plPos = playerPos world
+        stepsTaken = moveCount world
         newMap = updateMaze map plPos Path
         
         dir = changeDirection ev
         newPos = goToNeighbor map plPos dir
         newMap' = updateMaze newMap newPos Start
+        increase = incrementStep plPos newPos
