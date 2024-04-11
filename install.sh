7f5fb89d35e928491b70c29c6ddcc975e1b633ed
Victor Buendia <victorbuendia03@gmail.com>
2024-04-11 09:29:04 -0300

    Remove necessidade de confirmação na instalação de pacotes no Linux

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ if [ "$OSTYPE" = "Darwin" ]; then
     brew install haskell-stack
     brew install cabal-install
 elif [ "$OSTYPE" = "Linux" ]; then
-    sudo apt-get update
-    sudo apt-get install haskell-platform
-    sudo apt-get install cabal-install
+    sudo apt-get update -y
+    sudo apt-get install haskell-platform -y
+    sudo apt-get install cabal-install -y
     
     # Essas bibliotecas garantem que o OpenGL no sistema esteja de acordo com os requisitos para instalar o gloss
-    sudo apt-get install cmake pkg-config
-    sudo apt-get install mesa-utils libglu1-mesa-dev freeglut3-dev mesa-common-dev
-    sudo apt-get install libglew-dev libglfw3-dev libglm-dev
-    sudo apt-get install libao-dev libmpg123-dev
+    sudo apt-get install cmake pkg-config -y
+    sudo apt-get install mesa-utils libglu1-mesa-dev freeglut3-dev mesa-common-dev -y
+    sudo apt-get install libglew-dev libglfw3-dev libglm-dev -y
+    sudo apt-get install libao-dev libmpg123-dev -y
 else
     echo "Windows"
 fi
