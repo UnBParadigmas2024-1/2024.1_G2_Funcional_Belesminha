5d7cc8b811acb01ead89fa89bdadda51e5154814
Kevin Luis <k.luis360@gmail.com>
2024-04-12 10:43:14 -0300

    Adiciona leitura de ranking

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,41 @@
 import Graphics.Gloss
-
-import World (mazeToPicture,handleInput,initializeWorld,updateWorld)
+import System.IO
+import World (mazeToPicture, handleInput, initializeWorld, updateWorld)
 import Dijkstra (calculateMinSteps)
 import Maze (generateLeaves)
 import Map (mazeMap)
 
 main :: IO ()
 main = do
-  let initialMaze = mazeMap
+    -- Abrir o arquivo e ler o conteúdo
+    handle <- openFile "ranking.txt" ReadMode
+    contents <- hGetContents handle
+    let ranking = lines contents -- Separa as linhas do arquivo em uma lista de strings
 
-  leaves <- generateLeaves initialMaze
-  newWorld <- initializeWorld leaves
-  minSteps <- calculateMinSteps newWorld leaves initialMaze
+    -- Mostrar o ranking na tela
+    putStrLn "Ranking:"
+    mapM_ putStrLn ranking
+    putStrLn "Pressione Enter para iniciar o jogo."
 
-  play
-    (InWindow "Belesminha" (600, 600) (0, 0))
-    black
-    60
-    newWorld
-    (mazeToPicture minSteps)
-    handleInput
-    updateWorld
+    -- Fechar o arquivo após a leitura
+    hClose handle
+
+    -- Aguardar a entrada do jogador antes de iniciar o jogo
+    _ <- getLine
+
+    -- Resto do seu código
+    let initialMaze = mazeMap
+
+    leaves <- generateLeaves initialMaze
+    newWorld <- initializeWorld leaves
+    minSteps <- calculateMinSteps newWorld leaves initialMaze
+
+    -- Iniciar o jogo
+    play
+        (InWindow "Belesminha" (600, 600) (0, 0))
+        black
+        60
+        newWorld
+        (mazeToPicture minSteps)
+        handleInput
+        updateWorld
